a130882e1aa1cae9a30a2453bfb0b0cb0cf063ea
Mateusz Luczak <mateusz.luczak@cognifide.com>
2018-10-29 10:38:23 +0100

    Release 0.1.0

---
+++ release.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const semver = require('semver');
+const pkg = require('./package.json');
+const manifest = require('./manifest.json');
+
+const curVersion = pkg.version;
+
+(async () => {
+  const { newVersion } = await inquirer.prompt([{
+    type: 'input',
+    name: 'newVersion',
+    message: `Please provide a version (current: ${curVersion}):`,
+  }]);
+
+  if (!semver.valid(newVersion)) {
+    console.error(`Invalid version: ${newVersion}`);
+    process.exit(1)
+  }
+
+  if (semver.lt(newVersion, curVersion)) {
+    console.error(`New version (${newVersion}) cannot be lower than current version (${curVersion}).`);
+    process.exit(1);
+  }
+
+  const { yes } = await inquirer.prompt([{
+    name: 'yes',
+    message: `Release ${newVersion}?`,
+    type: 'confirm'
+  }]);
+
+  if (yes) {
+    pkg.version = newVersion;
+    manifest.version = newVersion;
+    manifest.version_name = newVersion;
+
+    fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2));
+    fs.writeFileSync('./manifest.json', JSON.stringify(manifest, null, 2));
+  } else {
+    process.exit(1);
+  }
+})()
