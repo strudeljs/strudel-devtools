5cd7b93887b6ea2e8365fd92eb3f61f18d99f3a4
Oliwer Iwanicki <oliwer.iwanicki@cognifide.com>
2018-11-23 16:04:20 +0100

    Undocked dev tools fix

--- src/core/aliases.js
+++ src/core/aliases.js
@@ -22,14 +22,14 @@ export const aliasCreators = {
 
 export const aliases = {};
 aliases[ALIAS_TYPES.SELECT_COMPONENT] = (data) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  chrome.tabs.query({ active: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { type: ALIAS_TYPES.SELECT_COMPONENT, id: data.id });
   });
   return selectComponent({ id: data.id });
 }
 
 aliases[ALIAS_TYPES.SCROLL_INTO_VIEW] = (data) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  chrome.tabs.query({ active: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { type: ALIAS_TYPES.SCROLL_INTO_VIEW, id: data.id });
   });
   return scrollIntoView({ id: data.id });
